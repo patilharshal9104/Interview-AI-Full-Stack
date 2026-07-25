e5b399016bdfc42c8ee6c3603bc97dcabfcd1b88
Harshal Patil <158460914+patilharshal9104@users.noreply.github.com>
2026-07-25 21:12:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interview-ai-full-stack-mjwl.onrender.com",
     credentials:true
 }))
 
